0282367922a5c903e4c361a61430564cbc9dcc86
Arto Eskelinen <arto.eske@gmail.com>
2023-06-06 10:05:12 +0300

    painikkeet + sivun muotoilua

--- toiminnot.js
+++ toiminnot.js
@@ -1,7 +1,15 @@
 let energia = 0;
 let laskuri = document.getElementById("laskuri");
+let kerroinPainike = document.getElementsByClassName("kerroinPainike").length;
 
 function klikkaus(){
     energia += 1;
-    laskuri.innerHTML = `Energian määrä: <b>${energia}</b>`
+    laskuri.innerHTML = `Energian määrä: <b>${energia}</b>`;
+}
+
+if(energia>4){
+    console.log("energiaa yli 5")
+    for(indeksi=0; indeksi<kerroinPainike; indeksi++){
+        document.getElementsByClassName("kerroinPainike")[indeksi].style.display = "inline-block";
+    }
 }
