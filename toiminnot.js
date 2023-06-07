ad9ce49dd901d482de74678cf4a947fc127089d1
Arto Eskelinen <arto.eske@gmail.com>
2023-06-07 08:28:30 +0300

    energiamäärän tarkastus

--- toiminnot.js
+++ toiminnot.js
@@ -3,13 +3,13 @@ let laskuri = document.getElementById("laskuri");
 let kerroinPainike = document.getElementsByClassName("kerroinPainike").length;
 
 function klikkaus(){
+    // lisää normaali määrä energiaa
     energia += 1;
     laskuri.innerHTML = `Energian määrä: <b>${energia}</b>`;
-}
 
-if(energia>4){
-    console.log("energiaa yli 5")
-    for(indeksi=0; indeksi<kerroinPainike; indeksi++){
-        document.getElementsByClassName("kerroinPainike")[indeksi].style.display = "inline-block";
+    // tarkista energian riittävyys kertoimeen
+    if(energia>4){
+        console.log("energiaa yli 5")
+        document.getElementById("ostaKerroin").style.display = "inline-block";
     }
 }
