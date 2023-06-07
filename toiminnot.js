51947a310a2dbbefd185dd26444bd2667205fb64
Arto Eskelinen <arto.eske@gmail.com>
2023-06-07 09:14:27 +0300

    uudet funktiot painikkeihin

--- toiminnot.js
+++ toiminnot.js
@@ -1,6 +1,7 @@
 let energia = 0;
 let laskuri = document.getElementById("laskuri");
-let kerroinPainike = document.getElementsByClassName("kerroinPainike").length;
+
+var tuplapisteet = false;
 
 function klikkaus(){
     // lisää normaali määrä energiaa
@@ -8,8 +9,21 @@ function klikkaus(){
     laskuri.innerHTML = `Energian määrä: <b>${energia}</b>`;
 
     // tarkista energian riittävyys kertoimeen
-    if(energia>4){
-        console.log("energiaa yli 5")
-        document.getElementById("ostaKerroin").style.display = "inline-block";
+    if(energia > 4 && tuplapisteet == false){
+        document.getElementById("kerroinPäälle").style.display = "inline-block";
     }
+}
+
+function kerroinOn(){
+    console.log("laitoit kertoimen päälle");
+    tuplapisteet = true;
+    document.getElementById("kerroinPäälle").style.display = "none";
+    document.getElementById("kerroinPois").style.display = "inline-block";
+}
+
+function kerroinOff(){
+    console.log("sammutit kertoimen");
+    tuplapisteet = false;
+    document.getElementById("kerroinPois").style.display = "none";
+    document.getElementById("kerroinPäälle").style.display = "inline-block";
 }
